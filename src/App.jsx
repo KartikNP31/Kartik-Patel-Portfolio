4e3661155a89c2022c1c956895a09a248b5bfa6d
KartikNP31 <patelkn1303@gmail.com>
2024-08-31 23:26:50 +0530

    Updates tech comp to lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { BrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
-import { About, Contact, Experience, Feedbacks, Navbar, Tech, Works, StarsCanvas, SocialMedia, Education, VolunteerExperience } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Navbar,
+  Works,
+  StarsCanvas,
+  SocialMedia,
+  Education,
+  VolunteerExperience,
+} from "./components";
 
-const Hero = lazy(() => import('./components/Hero'));
+const Hero = lazy(() => import("./components/Hero"));
+const Tech = lazy(() => import("./components/Tech"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className=' bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className=" bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Suspense fallback={<div className="text-black">Loading...</div>}>
             <Hero />
           </Suspense>
         </div>
-        <SocialMedia/>
+        <SocialMedia />
         <About />
-        <Education/>
+        <Education />
         <Experience />
         <Works />
-        <Tech />
+        <Suspense fallback={<div className="text-black">Loading...</div>}>
+          <Tech />
+        </Suspense>
         {/* <Feedbacks /> */}
-        <VolunteerExperience/>
-        <div className='relative z-0'>
+        <VolunteerExperience />
+        <div className="relative z-0">
           <Contact />
           {/* <StarsCanvas /> */}
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
